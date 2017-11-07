43f347cf8a8769ccd5cd8c87d081879f28bf3834
Axl <makosful@gmail.com>
2017-11-07 11:24:52 +0100

    Added example of getting all companies

--- src/inn2power/Inn2Power.java
+++ src/inn2power/Inn2Power.java
@@ -1,5 +1,10 @@
 package inn2power;
 
+import be.*;
+import dal.*;
+import java.io.IOException;
+import java.util.List;
+
 /**
  *
  * @author Axl
@@ -9,10 +14,29 @@ public class Inn2Power
 
     /**
      * @param args the command line arguments
+     *
+     * @throws java.io.IOException
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        getAllCompaniesExample();
+    }
 
+    /**
+     * An example on how to get the name of all companies individually
+     *
+     * @throws IOException
+     */
+    private static void getAllCompaniesExample() throws IOException
+    {
+        CompanyDAO cDAO = new CompanyDAO();
+
+        List<Company> companies = cDAO.getAllCompanies();
+
+        companies.forEach((company) ->
+        {
+            System.out.println(company.getName());
+        });
     }
 
 }
