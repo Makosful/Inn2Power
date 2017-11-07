ecb0d0b630027512b30b70162ede4c22966c7fca
Axl <makosful@gmail.com>
2017-11-07 10:07:33 +0100

    Removed comments

--- src/inn2power/Inn2Power.java
+++ src/inn2power/Inn2Power.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inn2power;
 
 /**
@@ -17,7 +12,7 @@ public class Inn2Power
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+
     }
 
 }
