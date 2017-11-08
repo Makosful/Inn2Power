7e59d130ee6bc1f7b49affe54df92fc3637aa557
dankphetamine <asge0907@easv365.dk>
2017-11-08 17:20:46 +0100

    Added auto-search on txtSearch input
    Automatically adjusts the TableView result(s) to the current input of txtSearch. Will update every single change (any char, space etc.)
    
    Also shortened unnecessary code and removed empty lines in other places.

--- src/inn2power/gui/controller/MainWindowController.java
+++ src/inn2power/gui/controller/MainWindowController.java
@@ -8,22 +8,26 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 /**
  *
  * @author B
  */
-public class MainWindowController implements Initializable
-{
+public class MainWindowController implements Initializable {
+
     @FXML
     private TableView<Company> tableView;
     @FXML
@@ -39,42 +43,51 @@ public class MainWindowController implements Initializable
     /*
      * @FXML private Button btnSearch;
      */
-    
     private ObservableList<Company> search = FXCollections.observableArrayList();
 
     BllManager bm = new BllManager();
 
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    public void initialize(URL url, ResourceBundle rb) {
+
+        AutoTextChange();
+
         tcName.setCellValueFactory(new PropertyValueFactory<>("Name:"));
         tcAddress.setCellValueFactory(new PropertyValueFactory<>("Address:"));
         tcId.setCellValueFactory(new PropertyValueFactory<>("ID:"));
 
-        try
-        {
+        try {
             tableView.setItems(bm.getAllCompaniesExample());
             tableView.getSortOrder().add(tcName);
             tableView.getSortOrder().add(tcAddress);
             tableView.getSortOrder().add(tcId);
-
-        }
-        catch (IOException ex)
-        {
+        } catch (IOException ex) {
             Logger.getLogger(MainWindowController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    private void AutoTextChange() {
+        txtSearch.textProperty().addListener(new ChangeListener<String>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends String> observable,
+                    String oldText, String newText) {
+                UpdatedSearch();
+                System.out.println("The text changed from: " + oldText + " to: " + newText);
+            }
+        });
+    }
+
     @FXML
-    private void btnSearchName(ActionEvent event) throws IOException
-    {
+    private void btnSearchName() throws IOException {
         tcName.setCellValueFactory(new PropertyValueFactory<>("name"));
         tcAddress.setCellValueFactory(new PropertyValueFactory<>("Address"));
         tcId.setCellValueFactory(new PropertyValueFactory<>("Id"));
-        
-        
+
+        UpdatedSearch();
+    }
+
+    private void UpdatedSearch() {
         tableView.setItems(bm.getSearchResult(txtSearch.getText()));
-        
-        //tableView.setItems(ip.getSearchResult(txtSearch.getText()));
     }
 }
