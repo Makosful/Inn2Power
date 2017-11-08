a138bc1ae0ddadad82e6463452a62ad40568ed27
Husso1997 <hussain1961@hotmail.com>
2017-11-08 11:11:58 +0100

    Added Country Filtering
    Filtering

---
+++ src/inn2power/bll/Filtering.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inn2power.bll;
+
+import be.Company;
+import dal.CompanyDAO;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Hussain
+ */
+public class Filtering 
+{
+    public void countrySearch() throws IOException
+    {
+        List<Company> allCompanies = new CompanyDAO().getAllCompanies();
+        
+        List<ICompanyFilter> filters = new ArrayList();
+        filters.add(new CountryFiltering("Phillipines"));
+        
+        List<Company> filteredList;
+        filteredList = companyFiltering(allCompanies, filters);
+        
+        for (Company company : filteredList)
+        {
+            System.out.println(company.getName());
+        }
+    }
+    
+    
+    public List<Company> companyFiltering(List<Company> allCompanies, List<ICompanyFilter> filters)
+    {
+        // ArrayList which countries are added to, according to if there is a 
+        // match. (Country match)
+        List<Company> filteredList = new ArrayList();
+         // Going through all companies
+        for (Company company : allCompanies)
+        {
+            boolean isThereAMatch = true;
+            
+            for (ICompanyFilter filter : filters)
+            {
+                if (!filter.meetCriteria(company))
+                {
+                    isThereAMatch = false;
+                    break;
+                }
+            } 
+            if(isThereAMatch)
+            {
+                filteredList.add(company);
+            }
+        }
+       
+        return filteredList;
+    }
+
+}
