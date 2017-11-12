0640303696cd94599c425b62d472f8be87122e43
davidmsf <davidmsf@gmail.com>
2017-11-12 13:36:16 +0100

    dataaccess in model

--- src/inn2power/gui/model/WindowModel.java
+++ src/inn2power/gui/model/WindowModel.java
@@ -1,5 +1,11 @@
 package inn2power.gui.model;
 
+import inn2power.bll.BllManager;
+import inn2power.dal.DataAccess;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Axl
@@ -7,7 +13,18 @@ package inn2power.gui.model;
 public class WindowModel
 {
 
+    private DataAccess data;
+   
+   
     public WindowModel()
     {
+          try
+        {
+            data = new DataAccess();
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(BllManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
