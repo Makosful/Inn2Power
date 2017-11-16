dc1604ac84b08dd262e7053b2aef05fdd0a9da54
Axl <makosful@gmail.com>
2017-11-16 19:26:12 +0100

    Added a much needed exception

---
+++ src/inn2power/bll/SteggerOverflowException.java
@@ -0,0 +1,19 @@
+package inn2power.bll;
+
+/**
+ *
+ * @author Axl
+ */
+public class SteggerOverflowException extends NullPointerException
+{
+
+    public SteggerOverflowException()
+    {
+        super();
+    }
+
+    public SteggerOverflowException(String message)
+    {
+        super(message);
+    }
+}
